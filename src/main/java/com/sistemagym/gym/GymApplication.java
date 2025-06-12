27687c038b34b4daa661e658adc76aab5fd4c011
juanescobar25 <juanescobardavid2007@gmail.com>
2025-06-12 17:40:49 -0500

    se envian todos los cambios

--- src/main/java/com/sistemagym/gym/GymApplication.java
+++ src/main/java/com/sistemagym/gym/GymApplication.java
@@ -1,5 +1,7 @@
 package com.sistemagym.gym;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GymApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GymApplication.class, args);
+		SpringApplication app = new SpringApplication(GymApplication.class);
+		String port = System.getenv("port");
+		if (port != null) {
+			app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		}
+		app.run(args);
 	}
 
 }
